83ead0ae024e4d2d8caa261023e941fd212bbe41
motasimfuad <motasimfuad0@gmail.com>
2022-02-11 21:03:17 +0600

    notebook model added

--- lib/data/models/notebook.dart
+++ lib/data/models/notebook.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:notebooks/data/models/label.dart';
@@ -19,7 +22,82 @@ class Notebook {
     this.isLocked = false,
   });
 
-  //
+  Notebook copyWith({
+    int? id,
+    String? name,
+    String? cover,
+    List<Label?>? labels,
+    List<Note?>? notes,
+    bool? isLocked,
+  }) {
+    return Notebook(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      cover: cover ?? this.cover,
+      labels: labels ?? this.labels,
+      notes: notes ?? this.notes,
+      isLocked: isLocked ?? this.isLocked,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'cover': cover,
+      'labels': labels?.map((x) => x?.toMap()).toList(),
+      'notes': notes?.map((x) => x?.toMap()).toList(),
+      'isLocked': isLocked,
+    };
+  }
+
+  factory Notebook.fromMap(Map<String, dynamic> map) {
+    return Notebook(
+      id: map['id']?.toInt(),
+      name: map['name'] ?? '',
+      cover: map['cover'] ?? '',
+      labels: map['labels'] != null
+          ? List<Label?>.from(map['labels']?.map((x) => Label?.fromMap(x)))
+          : null,
+      notes: map['notes'] != null
+          ? List<Note?>.from(map['notes']?.map((x) => Note?.fromMap(x)))
+          : null,
+      isLocked: map['isLocked'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Notebook.fromJson(String source) =>
+      Notebook.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Notebook(id: $id, name: $name, cover: $cover, labels: $labels, notes: $notes, isLocked: $isLocked)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Notebook &&
+        other.id == id &&
+        other.name == name &&
+        other.cover == cover &&
+        listEquals(other.labels, labels) &&
+        listEquals(other.notes, notes) &&
+        other.isLocked == isLocked;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        cover.hashCode ^
+        labels.hashCode ^
+        notes.hashCode ^
+        isLocked.hashCode;
+  }
 }
 
 // delete later

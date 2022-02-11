a51af41d96bfdf2d64592220c0a42363861f3fef
motasimfuad <motasimfuad0@gmail.com>
2022-02-11 21:12:10 +0600

    data repository for sqflite created

--- lib/data/repositories/data_repository.dart
+++ lib/data/repositories/data_repository.dart
@@ -1,7 +1,17 @@
 // Dummy file
 
+import 'package:sqflite/sqflite.dart';
+
 class DataRepository {
   DataRepository._private();
-
   static final DataRepository instance = DataRepository._private();
+
+  static Database? _database;
+
+  Future<Database?> get createDatabase async {
+    if (_database != null) {
+      return _database;
+    }
+    return _database;
+  }
 }

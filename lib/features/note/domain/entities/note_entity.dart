f39f6770c1c7f695a4892e2961e392d32f8f6c93
motasimfuad <motasimfuad0@gmail.com>
2022-05-30 18:09:13 +0600

    note entity added

---
+++ lib/features/note/domain/entities/note_entity.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../data/models/label.dart';
+
+class NoteEntity extends Equatable {
+  final int? id;
+  final String title;
+  final String description;
+  final bool isFavorite;
+  final bool? isLocked;
+  final Color color;
+  final DateTime? createdAt;
+  final int notebookId;
+  final List<Label>? labels;
+  const NoteEntity({
+    this.id,
+    required this.title,
+    required this.description,
+    required this.isFavorite,
+    this.isLocked,
+    required this.color,
+    this.createdAt,
+    required this.notebookId,
+    this.labels,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        description,
+        isFavorite,
+        isLocked,
+        color,
+        createdAt,
+        notebookId,
+        labels,
+      ];
+}

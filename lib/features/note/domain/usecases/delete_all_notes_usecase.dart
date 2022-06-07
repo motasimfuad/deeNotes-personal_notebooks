e665cc6fba48443f11664ffbfdf56669845cbffa
motasimfuad <motasimfuad0@gmail.com>
2022-06-07 09:44:49 +0600

    delete all notes usecase added

--- lib/features/note/domain/usecases/delete_all_notes_usecase.dart
+++ lib/features/note/domain/usecases/delete_all_notes_usecase.dart
@@ -1,1 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:notebooks/core/usecases/usecase.dart';
 
+import '../../../../core/error/failures.dart';
+import '../repositories/note_repository.dart';
+
+class GetAllNotesUsecase implements UseCase<int, Params> {
+  final NoteRepository repository;
+  GetAllNotesUsecase(this.repository);
+
+  @override
+  Future<Either<Failure, int>> call(Params params) async {
+    return await repository.deleteAllNotes(params.notebookId);
+  }
+}
+
+class Params extends Equatable {
+  final int notebookId;
+  const Params({
+    required this.notebookId,
+  });
+  @override
+  List<Object> get props => [notebookId];
+}

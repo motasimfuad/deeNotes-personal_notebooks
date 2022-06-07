45dc09e0939d082d39dea0d437512ac1c674d94e
motasimfuad <motasimfuad0@gmail.com>
2022-06-07 12:20:08 +0600

    note dependencies added

--- lib/dependency_injection.dart
+++ lib/dependency_injection.dart
@@ -1,5 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:notebooks/data/repositories/data_repository.dart';
+import 'package:notebooks/features/note/data/datasources/note_local_data_source.dart';
+import 'package:notebooks/features/note/domain/repositories/note_repository.dart';
+import 'package:notebooks/features/note/domain/usecases/delete_all_notes_usecase.dart';
+import 'package:notebooks/features/note/domain/usecases/delete_note_usecase.dart';
+import 'package:notebooks/features/note/domain/usecases/find_note_usecase.dart';
+import 'package:notebooks/features/note/domain/usecases/get_all_notes_usecase.dart';
+import 'package:notebooks/features/note/presentation/bloc/note_bloc.dart';
 import 'package:notebooks/features/notebook/data/datasources/notebook_local_data_source.dart';
 import 'package:notebooks/features/notebook/data/repositories/notebook_repository_impl.dart';
 import 'package:notebooks/features/notebook/domain/repositories/notebook_repository.dart';
@@ -11,36 +18,67 @@ import 'package:notebooks/features/notebook/domain/usecases/get_all_notebooks_us
 import 'package:notebooks/features/notebook/domain/usecases/update_notebook_usecase.dart';
 import 'package:notebooks/features/notebook/presentation/bloc/notebook_bloc.dart';
 
+import 'features/note/data/repositories/note_repository_impl.dart';
+import 'features/note/domain/usecases/create_note_usecase.dart';
+import 'features/note/domain/usecases/update_note_usecase.dart';
+
 final getIt = GetIt.instance;
 
 Future<void> init() async {
   //! Features
   // Blocs
-  getIt.registerFactory(() => NotebookBloc(
-        getAllNotebooks: getIt(),
-        createNotebook: getIt(),
-        findNotebook: getIt(),
-        updateNotebook: getIt(),
-        deleteNotebook: getIt(),
-        deleteAllNotebooks: getIt(),
-      ));
+  getIt.registerFactory(
+    () => NotebookBloc(
+      getAllNotebooks: getIt(),
+      createNotebook: getIt(),
+      findNotebook: getIt(),
+      updateNotebook: getIt(),
+      deleteNotebook: getIt(),
+      deleteAllNotebooks: getIt(),
+    ),
+  );
+
+  getIt.registerFactory(
+    () => NoteBloc(
+      createNote: getIt(),
+      updateNote: getIt(),
+      findNote: getIt(),
+      getAllNotes: getIt(),
+      deleteNote: getIt(),
+      deleteAllNotes: getIt(),
+    ),
+  );
 
   // UseCases
+  // notebook usecases
   getIt.registerLazySingleton(() => GetAllNotebooksUsecase(getIt()));
   getIt.registerLazySingleton(() => CreateNotebookUsecase(getIt()));
   getIt.registerLazySingleton(() => FindNotebookUsecase(getIt()));
   getIt.registerLazySingleton(() => UpdateNotebookUsecase(getIt()));
   getIt.registerLazySingleton(() => DeleteNotebookUsecase(getIt()));
   getIt.registerLazySingleton(() => DeleteAllNotebooksUsecase(getIt()));
+  // note usecases
+  getIt.registerLazySingleton(() => CreateNoteUsecase(getIt()));
+  getIt.registerLazySingleton(() => UpdateNoteUsecase(getIt()));
+  getIt.registerLazySingleton(() => DeleteNoteUsecase(getIt()));
+  getIt.registerLazySingleton(() => GetAllNotesUsecase(getIt()));
+  getIt.registerLazySingleton(() => FindNoteUsecase(getIt()));
+  getIt.registerLazySingleton(() => DeleteAllNotesUsecase(getIt()));
 
   // Repositories
   getIt.registerLazySingleton<NotebookRepository>(
-    () => NotebookRepositoryImpl(getIt()),
-  );
+      () => NotebookRepositoryImpl(getIt()));
+
+  getIt.resetLazySingleton<NoteRepository>(
+      instance: () => NoteRepositoryImpl(getIt()));
 
   //data sources
   getIt.registerLazySingleton<NotebookLocalDataSource>(
     () => NotebookLocalDataSourceImpl(dataRepo: getIt()),
+  );
+
+  getIt.registerLazySingleton<NoteLocalDataSource>(
+    () => NoteLocalDataSourceImpl(dataRepo: getIt()),
   );
 
   //! core
